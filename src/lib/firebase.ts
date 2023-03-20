30044ff1eeff3f8009b1bc52efe5a27342f81e56
Eren <eren.olympic.14@gmail.com>
2023-03-20 10:55:27 +0800

    initial commit

---
+++ src/lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAz2pEW8h7msBt-_LwMX1ANhU3pduLaYlg",
+  authDomain: "genshin-calculator-4da5e.firebaseapp.com",
+  projectId: "genshin-calculator-4da5e",
+  storageBucket: "genshin-calculator-4da5e.appspot.com",
+  messagingSenderId: "398255414420",
+  appId: "1:398255414420:web:bd31687fcea57fb646a881"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
